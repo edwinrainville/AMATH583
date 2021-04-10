a2a1f7ddf49cc5b79932375a13b5b1bbedf79ac7
edwinrainville <erainvil@uw.edu>
2021-04-09 17:23:20 -0700

    5.2 - Added hello_world.cpp
    Added hello_world.cpp which prints out "I am 3.2 kilometers from Lewis Hall"

---
+++ ps2/hello_world.cpp
@@ -0,0 +1,11 @@
+// Hello World Program 
+// Written By: EJ Rainville, Spring 2021
+// Description: Outputs distance to Lewis Hall in Kilometers
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "I am 3.2 kilometers from Lewis Hall" << endl;
+  return 0;
+}
